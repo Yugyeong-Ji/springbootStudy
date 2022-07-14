a5d534be561fe791490e487faaab2c5f3baf338a
Yugyeong-Ji <2monica9@naver.com>
2022-07-14 21:28:02 +0900

    :bulb: add comments in source code

--- src/main/java/posts/Posts.java
+++ src/main/java/posts/Posts.java
@@ -10,12 +10,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter
-@NoArgsConstructor
-@Entity
+@Getter//클래스 내 모든 필드의 Getter 메소드를 자동생성
+@NoArgsConstructor//기본 생성자 자동 추가
+@Entity//테이블과 링크될 클래스임을 나타냄
 public class Posts {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id //기본키(PK)필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//PK의 생성규칙을 나타냄
     private Long id;
 
     @Column(length = 500, nullable = false)
@@ -26,7 +26,7 @@ public class Posts {
 
     private String author;
 
-    @Builder
+    @Builder//해당 클래스의 빌더 패턴 클래스를 생성
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
